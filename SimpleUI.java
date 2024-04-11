48fa328d14005cda26d69ceb8f690380cb9615e0
cyberwiz <115326337+MahriYalkapova@users.noreply.github.com>
2024-04-11 14:29:27 -0700

    Added a simple UI class that creates a gui window

---
+++ SimpleUI.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+public class SimpleUI {
+    public static void main(String args[]){
+        JFrame frame = new JFrame("Javavengers");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300,300);
+        frame.setVisible(true);
+    }
+}
