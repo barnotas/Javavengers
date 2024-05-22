dcfa30eb999b5fdb378794b8f7fa2d2c09a08262
cyberwiz <115326337+MahriYalkapova@users.noreply.github.com>
2024-05-22 00:07:52 -0700

    Added logi for creating a new project

--- view/ProjectsPanel.java
+++ view/ProjectsPanel.java
@@ -1,28 +1,34 @@
 package view;
 
+import controller.NewProjectController;
+import model.NewProjectModel;
+
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-// I added
 
-public class ProjectsPanel extends JPanel implements ActionListener {
+public class ProjectsPanel extends JPanel{
     private JLabel projectNameLabel ;
     private JLabel projectDescriptionLabel;
+    private JButton newProjectButton;
+    private NewProjectController newProjectController;
     public ProjectsPanel() {
-        //setLayout(new GridBagLayout());
+
         projectNameLabel = new JLabel();
         projectDescriptionLabel = new JLabel();
+        newProjectButton = new JButton("Add new project");
         add(projectNameLabel);
         add(projectDescriptionLabel);
         add(new JLabel("Projects"));
         add(new JLabel("This is where you can manage your projects."));
        // GridBagConstraints constraints = new GridBagConstraints();
         //constraints.fill = GridBagConstraints.HORIZONTAL;
-        add(new JButton("Add new project"));
-        
+        newProjectButton.addNewProjectListener(new ActionListener());
     }
 
 
@@ -33,10 +39,8 @@ public class ProjectsPanel extends JPanel implements ActionListener {
     public void setProjectDescription(String description) {
         projectDescriptionLabel.setText("Project Description: " + description);
     }
-    
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-
-     }
+    public void addNewProjectListener(ActionListener listener) {
+        newProjectButton.addActionListener(listener);
     }
+}
