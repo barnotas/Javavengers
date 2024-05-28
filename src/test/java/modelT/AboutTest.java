f35304f80c7b9ad8dafa87d6e13f92c05c9a9514
AM-H <Ahmedm.muse469@gmail.com>
2024-05-28 16:38:38 -0700

    Merge branch 'Ahmed-re

--- src/test/java/modelT/AboutTest.java
+++ src/test/java/modelT/AboutTest.java
@@ -1,22 +1,23 @@
-//package test.java.modelT;
-//
-//import model.About;
-//import org.junit.jupiter.*;
-//import org.junit.jupiter.api.Test;
-//import org.testng.Assert;
-//import org.junit.*;
-//
-///**
+// package test.java.modelT;
+
+// import model.About;
+
+// import org.junit.jupiter.*;
+// import org.junit.jupiter.api.Test;
+// import org.testng.Assert;
+// import org.junit.*;
+
+// /**
 // * Unit tests for the About class.
 // *
 // * @author Bernard Bega
 // * @version 1.0
 // */
-//public class AboutTest {
-//
+// public class AboutTest {
+
 //    /** An About object used for testing. */
 //    private final About A = new About();
-//
+
 //    /**
 //     * Tests the getVersion method.
 //     */
@@ -24,7 +25,7 @@
 //    public void testGetVersion() {
 //        Assert.assertEquals(0.1, A.getVersion());
 //    }
-//
+
 //    /**
 //     * Tests the getDevTeam method.
 //     */
@@ -32,7 +33,7 @@
 //    public void testGetDevTeam() {
 //        Assert.assertEquals("Javavengers", A.getDevTeam());
 //    }
-//
+
 //    /**
 //     * Tests the getDevs method.
 //     */
@@ -41,4 +42,4 @@
 //        Assert.assertEquals(new String[]{"Ahmed Hassan", "Barno Tashpulatova",
 //                        "Bernard Bega", "Mahri Yalkapova"}, A.getDevs());
 //    }
-//}
+// }
