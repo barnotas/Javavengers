a4bf1cc13eb6883d0a139045ae108175d23243fb
AM-H <Ahmedm.muse469@gmail.com>
2024-05-30 13:54:39 -0700

    little changes

--- src/main/java/view/ProjectsPanel.java
+++ src/main/java/view/ProjectsPanel.java
@@ -6,17 +6,70 @@ import controller.ProjectController;
 import javax.swing.*;
 import java.awt.*;
 
+
+/**
+ * The ProjectsPanel class represents a panel for displaying and managing projects.
+ * It provides a list of projects, project details, and buttons for adding new projects.
+ */
+
 public class ProjectsPanel extends JPanel {
+    
+    /**
+     * The JLabel for displaying the project name.
+     */
     private JLabel projectNameLabel;
+
+    /**
+     * The JLabel for displaying the project description.
+     */
     private JLabel projectDescriptionLabel;
+<<<<<<< HEAD
+=======
+
+    /**
+     * The JButton for adding a new project.
+     */
+    private JButton newProjectButton;
+
+    /**
+     * The NewProjectController instance for handling the creation of new projects.
+     */
+    private NewProjectController newProjectController;
+
+    /**
+     * The JList for displaying the list of projects.
+     */
+>>>>>>> 99b2288 (little changes)
     private JList<String> projectList;
+
+    /**
+     * The DefaultListModel for storing the project list items.
+     */
     private DefaultListModel<String> projectListModel;
+
+    /**
+     * The JLabel for displaying the project budget.
+     */
     private JLabel budgetLabel;
+
+    /**
+     * The JLabel for displaying the project expenses.
+     */
     private JLabel expensesLabel;
     private ProjectController projectController;
 
+<<<<<<< HEAD
     public ProjectsPanel(ProjectController projectController) {
         this.projectController = projectController;
+=======
+    /**
+     * Constructs a new ProjectsPanel with the specified NewProjectController.
+     *
+     * @param projectController the NewProjectController instance for handling the creation of new projects
+     */
+
+    public ProjectsPanel(NewProjectController projectController) {
+>>>>>>> 99b2288 (little changes)
         setLayout(new BorderLayout());
 
         // Create a panel for the project list
@@ -106,22 +159,44 @@ public class ProjectsPanel extends JPanel {
         repaint();
     }
 
+
+     /**
+     * Sets the project name to be displayed.
+     *
+     * @param name the name of the project
+     */
     public void setProjectName(String name) {
         projectNameLabel.setText("Project Name: " + name);
     }
 
+     /**
+     * Sets the project name to be displayed.
+     *
+     * @param name the name of the project
+     */
     public void setProjectDescription(String description) {
         projectDescriptionLabel.setText("Project Description: " + description);
     }
 
+    /**
+     * Sets the project budget to be displayed.
+     *
+     * @param budget the budget of the project
+     */
     public void setBudget(double budget) {
         budgetLabel.setText("Budget: $" + budget);
     }
 
+    /**
+     * Sets the project expenses to be displayed.
+     *
+     * @param expenses the expenses of the project
+     */
     public void setExpenses(double expenses) {
         expensesLabel.setText("Expenses: $" + expenses);
     }
 
+<<<<<<< HEAD
     public void updateProject(int index, Project project) {
         String listEntry = "Project Name: " + project.getName() + " - Description: " + project.getDescription() +
                 " - Budget: $" + project.getBudget() + " - Expenses: $" + project.getExpenses();
@@ -143,5 +218,50 @@ public class ProjectsPanel extends JPanel {
         if (index != -1) {
             projectListModel.remove(index);
         }
+=======
+    /**
+     * Adds a new project to the project list.
+     *
+     * @param projectName the name of the project to be added
+     */
+    public void addProject(String projectName) {
+        projectListModel.addElement(projectName);
+        revalidate();
+        repaint();
+    }
+
+     /**
+     * Adds a new project to the project list.
+     *
+     * @param project the Project object to be added
+     */
+       public void addProject(Project project) {
+        String listEntry = project.getName() + " - Description: " + project.getDescription();
+        projectListModel.addElement(listEntry);
+        revalidate();
+        repaint();
+    }
+
+    /**
+     * Adds a new project to the project list.
+     *
+     * @param projectName the name of the project to be added
+     * @param projectDescription the description of the project to be added
+     */
+    public void addProject(String projectName, String projectDescription) {
+        String listEntry = projectName + " - Description: " + projectDescription;
+        projectListModel.addElement(listEntry);
+        revalidate();
+        repaint();
+    }
+
+    /**
+     * Sets the NewProjectController instance for handling the creation of new projects.
+     *
+     * @param newProjectController the NewProjectController instance to be set
+     */
+    public void setNewProjectController(NewProjectController newProjectController) {
+        this.newProjectController = newProjectController;
+>>>>>>> 99b2288 (little changes)
     }
 }
