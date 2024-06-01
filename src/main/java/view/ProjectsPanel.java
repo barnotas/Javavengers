82eeb788d1f7b5ab8116338f1b66050b5b1c2b97
AM-H <Ahmedm.muse469@gmail.com>
2024-06-01 02:17:28 -0700

    Projects panel works & needs to connect to Users

--- src/main/java/view/ProjectsPanel.java
+++ src/main/java/view/ProjectsPanel.java
@@ -1,68 +1,22 @@
 package view;
 
-import controller.NewProjectController;
-import model.NewProjectModel;
 import model.Project;
-import model.ProjectList;
+import controller.ProjectController;
 
 import javax.swing.*;
 import java.awt.*;
 
-
-/**
- * The ProjectsPanel class represents a panel for displaying and managing projects.
- * It provides a list of projects, project details, and buttons for adding new projects.
- */
-
 public class ProjectsPanel extends JPanel {
-    
-    /**
-     * The JLabel for displaying the project name.
-     */
     private JLabel projectNameLabel;
-
-    /**
-     * The JLabel for displaying the project description.
-     */
     private JLabel projectDescriptionLabel;
-
-    /**
-     * The JButton for adding a new project.
-     */
-    private JButton newProjectButton;
-
-    /**
-     * The NewProjectController instance for handling the creation of new projects.
-     */
-    private NewProjectController newProjectController;
-
-    /**
-     * The JList for displaying the list of projects.
-     */
     private JList<String> projectList;
-
-    /**
-     * The DefaultListModel for storing the project list items.
-     */
     private DefaultListModel<String> projectListModel;
-
-    /**
-     * The JLabel for displaying the project budget.
-     */
     private JLabel budgetLabel;
-
-    /**
-     * The JLabel for displaying the project expenses.
-     */
     private JLabel expensesLabel;
+    private ProjectController projectController;
 
-    /**
-     * Constructs a new ProjectsPanel with the specified NewProjectController.
-     *
-     * @param projectController the NewProjectController instance for handling the creation of new projects
-     */
-
-    public ProjectsPanel(NewProjectController projectController) {
+    public ProjectsPanel(ProjectController projectController) {
+        this.projectController = projectController;
         setLayout(new BorderLayout());
 
         // Create a panel for the project list
@@ -72,122 +26,122 @@ public class ProjectsPanel extends JPanel {
         projectListPanel.add(new JScrollPane(projectList), BorderLayout.CENTER);
         add(projectListPanel, BorderLayout.WEST);
 
-        // Create a panel for the project details
-        JPanel projectDetailsPanel = new JPanel(new GridBagLayout());
-        GridBagConstraints constraints = new GridBagConstraints();
-        constraints.gridx = 0;
-        constraints.gridy = 0;
-        constraints.anchor = GridBagConstraints.WEST;
-        constraints.insets = new Insets(5, 5, 5, 5);
-
-        projectNameLabel = new JLabel();
-        projectDetailsPanel.add(projectNameLabel, constraints);
-
-        constraints.gridy = 1;
-        projectDescriptionLabel = new JLabel();
-        projectDetailsPanel.add(projectDescriptionLabel, constraints);
-
-        constraints.gridy = 2;
-        budgetLabel = new JLabel();
-        projectDetailsPanel.add(budgetLabel, constraints);
-
-        constraints.gridy = 3;
-        expensesLabel = new JLabel();
-        projectDetailsPanel.add(expensesLabel, constraints);
-
-        add(projectDetailsPanel, BorderLayout.CENTER);
+       // Create a button to open the "Create a new project" dialog
+        JButton newProjectButton = new JButton("Add New Project");
+        newProjectButton.addActionListener(e -> showCreateNewProjectDialog());
 
         // Create a panel for the buttons
         JPanel buttonPanel = new JPanel();
-        newProjectButton = new JButton("Add new project");
         buttonPanel.add(newProjectButton);
-        add(buttonPanel, BorderLayout.SOUTH);
-
-        newProjectController = projectController;
-        newProjectButton.addActionListener(e -> {
-            newProjectController.showNewProjectPopup(SwingUtilities.getWindowAncestor(ProjectsPanel.this));
-        });
+        add(buttonPanel, BorderLayout.NORTH);
     }
 
 
-     /**
-     * Sets the project name to be displayed.
-     *
-     * @param name the name of the project
-     */
+
+    private void showCreateNewProjectDialog() {
+        JDialog newProjectDialog = new JDialog(SwingUtilities.getWindowAncestor(this), "Create New Project");
+        newProjectDialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        newProjectDialog.setLayout(new BorderLayout());
+    
+        // Create a panel for creating a new project
+        JPanel newProjectPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints newProjectConstraints = new GridBagConstraints();
+        newProjectConstraints.gridx = 0;
+        newProjectConstraints.gridy = 0;
+        newProjectConstraints.anchor = GridBagConstraints.WEST;
+        newProjectConstraints.insets = new Insets(5, 5, 5, 5);
+    
+        JLabel projectNameLabel = new JLabel("Project Name:");
+        newProjectPanel.add(projectNameLabel, newProjectConstraints);
+    
+        newProjectConstraints.gridx = 1;
+        JTextField projectNameField = new JTextField(20);
+        newProjectPanel.add(projectNameField, newProjectConstraints);
+    
+        newProjectConstraints.gridx = 0;
+        newProjectConstraints.gridy = 1;
+        JLabel projectDescriptionLabel = new JLabel("Project Description:");
+        newProjectPanel.add(projectDescriptionLabel, newProjectConstraints);
+    
+        newProjectConstraints.gridx = 1;
+        JTextArea projectDescriptionArea = new JTextArea(4, 20);
+        JScrollPane projectDescriptionScrollPane = new JScrollPane(projectDescriptionArea);
+        newProjectPanel.add(projectDescriptionScrollPane, newProjectConstraints);
+    
+        newProjectConstraints.gridx = 0;
+        newProjectConstraints.gridy = 2;
+        JLabel projectBudgetLabel = new JLabel("Project Budget:");
+        newProjectPanel.add(projectBudgetLabel, newProjectConstraints);
+    
+        newProjectConstraints.gridx = 1;
+        JTextField projectBudgetField = new JTextField(10);
+        newProjectPanel.add(projectBudgetField, newProjectConstraints);
+    
+        newProjectDialog.add(newProjectPanel, BorderLayout.CENTER);
+    
+        // Create a panel for the dialog buttons
+        JPanel dialogButtonPanel = new JPanel();
+        JButton createButton = new JButton("Create");
+        createButton.addActionListener(e -> {
+            String projectName = projectNameField.getText();
+            String projectDescription = projectDescriptionArea.getText();
+            double projectBudget = Double.parseDouble(projectBudgetField.getText());
+            Project project = new Project(projectName, projectDescription, projectBudget);
+            projectController.createProject(project);
+            newProjectDialog.dispose();
+        });
+        dialogButtonPanel.add(createButton);
+    
+        newProjectDialog.add(dialogButtonPanel, BorderLayout.SOUTH);
+    
+        newProjectDialog.pack();
+        newProjectDialog.setLocationRelativeTo(SwingUtilities.getWindowAncestor(this));
+        newProjectDialog.setVisible(true);
+    }
+
+    public void addProject(Project project) {
+        String listEntry = "Project Name: " + project.getName() + " - Description: " + project.getDescription() + " - Budget: $" + project.getBudget() + " - Expenses: $" + project.getExpenses();
+        projectListModel.addElement(listEntry);
+        revalidate();
+        repaint();
+    }
+
     public void setProjectName(String name) {
         projectNameLabel.setText("Project Name: " + name);
     }
 
-     /**
-     * Sets the project name to be displayed.
-     *
-     * @param name the name of the project
-     */
     public void setProjectDescription(String description) {
         projectDescriptionLabel.setText("Project Description: " + description);
     }
 
-    /**
-     * Sets the project budget to be displayed.
-     *
-     * @param budget the budget of the project
-     */
     public void setBudget(double budget) {
         budgetLabel.setText("Budget: $" + budget);
     }
 
-    /**
-     * Sets the project expenses to be displayed.
-     *
-     * @param expenses the expenses of the project
-     */
     public void setExpenses(double expenses) {
         expensesLabel.setText("Expenses: $" + expenses);
     }
 
-    /**
-     * Adds a new project to the project list.
-     *
-     * @param projectName the name of the project to be added
-     */
-    public void addProject(String projectName) {
-        projectListModel.addElement(projectName);
-        revalidate();
-        repaint();
+    public void updateProject(int index, Project project) {
+        String listEntry = "Project Name: " + project.getName() + " - Description: " + project.getDescription() +
+                " - Budget: $" + project.getBudget() + " - Expenses: $" + project.getExpenses();
+        projectListModel.setElementAt(listEntry, index);
     }
-
-     /**
-     * Adds a new project to the project list.
-     *
-     * @param project the Project object to be added
-     */
-       public void addProject(Project project) {
-        String listEntry = project.getName() + " - Description: " + project.getDescription();
-        projectListModel.addElement(listEntry);
-        revalidate();
-        repaint();
-    }
-
-    /**
-     * Adds a new project to the project list.
-     *
-     * @param projectName the name of the project to be added
-     * @param projectDescription the description of the project to be added
-     */
-    public void addProject(String projectName, String projectDescription) {
-        String listEntry = projectName + " - Description: " + projectDescription;
-        projectListModel.addElement(listEntry);
-        revalidate();
-        repaint();
-    }
-
-    /**
-     * Sets the NewProjectController instance for handling the creation of new projects.
-     *
-     * @param newProjectController the NewProjectController instance to be set
-     */
-    public void setNewProjectController(NewProjectController newProjectController) {
-        this.newProjectController = newProjectController;
+    
+    public void removeProject(Project project) {
+        // Find the index of the project in the list model
+        int index = -1;
+        for (int i = 0; i < projectListModel.getSize(); i++) {
+            String listEntry = projectListModel.getElementAt(i);
+            if (listEntry.startsWith("Project Name: " + project.getName() + " - ")) {
+                index = i;
+                break;
+            }
+        }
+    
+        // Remove the project from the list model if found
+        if (index != -1) {
+            projectListModel.remove(index);
+        }
     }
 }
