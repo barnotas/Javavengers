f412c333c47b9068127401aea46e8e5865587da7
Barnotas <barnotas@gmail.com>
2024-06-06 13:43:24 -0700

    Java docs

--- src/main/java/model/ProjectList.java
+++ src/main/java/model/ProjectList.java
@@ -1,18 +1,34 @@
 package model;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * This class created list of projects
+ * @author Bernard Bega, Barno Tashpulatova, Ahmed Hassan, Mahri Yalkapova
+ */
 
 public class ProjectList {
+    /** A field for project list. */
     private List<Project> projects;
 
+    /**
+     * Constructs project list class.
+     */
     public ProjectList() {
         projects = new ArrayList<>();
     }
 
+    /**
+     * Adds projects to the list.
+     * @param project
+     */
     public void addProject(Project project) {
         projects.add(project);
     }
 
+    /** 
+     * Returns project list.
+     */
     public List<Project> getProjects () {
         return projects;
     }
