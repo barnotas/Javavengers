6ac7bed580195f0fee714813844062a261adff1f
Barnotas <barnotas@gmail.com>
2024-06-06 16:17:14 -0700

    Added some java doc

--- src/main/java/view/SettingsPanel.java
+++ src/main/java/view/SettingsPanel.java
@@ -1,15 +1,29 @@
 package view;
 
-import javax.swing.*;
-import java.awt.*;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+/**
+ * The SettingsPanel class represents a panel for displaying user settings.
+ * It provides fields for displaying username and email, as well as buttons for exporting and importing data.
+ */
 public class SettingsPanel extends JPanel {
+    /** A field for username label. */
     private JLabel usernameLabel;
+    /** A field for email label. */
     private JLabel emailLabel;
+    /** A field for export button. */
     private JButton exportButton;
+    /** A field for import button. */
     private JButton importButton;
-
+    /**
+     * Constructs a SettingsPanel.
+     */
     public SettingsPanel() {
         setLayout(new GridBagLayout());
         GridBagConstraints constraints = new GridBagConstraints();
@@ -44,19 +58,31 @@ public class SettingsPanel extends JPanel {
         constraints.gridwidth = 2;
         add(buttonPanel, constraints);
     }
-
+    /**
+     * Sets the username to be displayed.
+     * @param username the username
+     */
     public void setUsername(String username) {
         usernameLabel.setText(username);
     }
-
+    /**
+     * Sets the email to be displayed.
+     * @param email the email
+     */
     public void setEmail(String email) {
         emailLabel.setText(email);
     }
-
+    /**
+     * Adds an ActionListener to the export button.
+     * @param listener the ActionListener
+     */
     public void addExportButtonListener(ActionListener listener) {
         exportButton.addActionListener(listener);
     }
-
+    /**
+     * Adds an ActionListener to the import button.
+     * @param listener the ActionListener
+     */
     public void addImportButtonListener(ActionListener listener) {
         importButton.addActionListener(listener);
     }
