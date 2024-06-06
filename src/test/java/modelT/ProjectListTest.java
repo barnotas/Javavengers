2582f8092392e1cc7f164ed6aaf9990cc237df9c
BernieBot <bbega@uw.edu>
2024-06-06 11:33:06 -0700

    Create ProjectListTest.java

---
+++ src/test/java/modelT/ProjectListTest.java
@@ -0,0 +1,33 @@
+package modelT;
+
+import model.ProjectList;
+import model.Project;
+
+/**
+ * Unit tests for the ProjectList class.
+ *
+ * @author Bernard Bega
+ * @version 1.0
+ */
+public class ProjectListTest {
+
+    /**
+     * Tests the ProjectList constructor.
+     */
+    @Test
+    public void testProjectListConstructor() {
+        ProjectList pl = new ProjectList();
+        Assert.assertEquals(0, pl.getProjects().size());
+    }
+
+    /**
+     * Tests the addProject method.
+     */
+    @Test
+    public void testAddProject() {
+        ProjectList pl = new ProjectList();
+        Project p = new Project("Ha!", "I don't know what this is.", 49.94);
+        pl.addProject(p);
+        Assert.assertEquals(1, pl.getProjects().size());
+    }
+}
