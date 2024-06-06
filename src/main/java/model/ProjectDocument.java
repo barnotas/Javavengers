a387d339cc3c99129f44fe2b9e00fe777a0e1d14
Barnotas <barnotas@gmail.com>
2024-06-06 14:44:07 -0700

    Added some java

--- src/main/java/model/ProjectDocument.java
+++ src/main/java/model/ProjectDocument.java
@@ -1,18 +1,40 @@
 package model;
 
+/**
+ * The ProjectDocument class represents a document associated with a project.
+ * It contains the name and filepath of the document.
+ * 
+ * @autor Bernard Bega, Barno Tashpulatova, Ahmed Hassan, Mahri Yalkapova
+ */
 public class ProjectDocument {
+    /** A field for name. */
     private String name;
+    /** A field for file path. */
     private String filepath;
-
+    
+    /**
+     * Constructs a ProjectDocument with the specified name and filepath.
+     * 
+     * @param name the name of the document
+     * @param filepath the filepath of the document
+     */
     public ProjectDocument(String name, String filepath) {
         this.name = name;
         this.filepath = filepath;
     }
 
+    /**
+     * Returns file path.
+     * @return
+     */
     public String getFilepath() {
         return filepath;
     }
 
+    /**
+     * Returns name of file.
+     * @return
+     */
     public String name() {
         return name;
     }
