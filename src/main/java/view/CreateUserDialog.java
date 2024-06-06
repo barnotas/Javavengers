eee09c1f54e730aa0368444567800c142f36775c
Barnotas <barnotas@gmail.com>
2024-06-06 15:20:52 -0700

    Added some java docs

--- src/main/java/view/CreateUserDialog.java
+++ src/main/java/view/CreateUserDialog.java
@@ -1,21 +1,48 @@
 package view;
 
-import controller.UserController;
-import model.User;
-
-import javax.swing.*;
-import java.awt.*;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.util.regex.Pattern;
 
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+import controller.UserController;
+import model.User;
+/**
+ * CreateUserDialog is a modal dialog for creating a new user.
+ * It provides input fields for username, password, email, and first name.
+ * It also includes validation for the email format.
+ * 
+ * @autor Bernard Bega, Barno Tashpulatova, Ahmed Hassan, Mahri Yalkapova
+ */
 public class CreateUserDialog extends JDialog {
+    /** A field for user controller. */
     private UserController userController;
+    /** A field for username.*/
     private JTextField usernameField;
+    /** A field for JPassword. */
     private JPasswordField passwordField;
+    /** A field for email field. */
     private JTextField emailField;
+    /** A field for first name field. */
     private JTextField firstNameField;
 
+    /** A constant for email pattern. */
     private static final Pattern EMAIL_PATTERN = Pattern.compile("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]+$");
 
+    /**
+     * Constructs a CreateUserDialog with the specified UserController.
+     *
+     * @param userController the user controller to manage user operations
+     */
     public CreateUserDialog(UserController userController) {
         this.userController = userController;
         setTitle("Create User");
@@ -27,6 +54,9 @@ public class CreateUserDialog extends JDialog {
         setLocationRelativeTo(null);
     }
 
+    /**
+     * Initializes components of JPanel.
+     */
     private void initComponents() {
         JPanel panel = new JPanel(new GridBagLayout());
         GridBagConstraints constraints = new GridBagConstraints();
@@ -97,6 +127,9 @@ public class CreateUserDialog extends JDialog {
     }
 
 
+    /**
+     * Creates user button.
+     */
     private void createUserButtonClicked() {
         String username = usernameField.getText();
         String password = new String(passwordField.getPassword());
@@ -120,6 +153,11 @@ public class CreateUserDialog extends JDialog {
         }
     }
 
+    /**
+     * Checks if email pattern matches entered details.
+     * @param email
+     * @return
+     */
     private boolean isValidEmail(String email) {
         return EMAIL_PATTERN.matcher(email).matches();
     }
