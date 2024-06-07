82e9ec11f5c7d8ccd3e945f2b6cef74affc03278
cyberwiz <115326337+MahriYalkapova@users.noreply.github.com>
2024-06-06 20:23:22 -0700

    Changed ProjectDocument

--- src/main/java/model/ProjectDocument.java
+++ src/main/java/model/ProjectDocument.java
@@ -7,7 +7,37 @@ package model;
  * @autor Bernard Bega, Barno Tashpulatova, Ahmed Hassan, Mahri Yalkapova
  */
 public class ProjectDocument {
+    /** A field for name. */
+    private String name;
+    /** A field for file path. */
+    private String filepath;
     
+    /**
+     * Constructs a ProjectDocument with the specified name and filepath.
+     * 
+     * @param name the name of the document
+     * @param filepath the filepath of the document
+     */
+    public ProjectDocument(String name, String filepath) {
+        this.name = name;
+        this.filepath = filepath;
+    }
+
+    /**
+     * Returns file path.
+     * @return
+     */
+    public String getFilepath() {
+        return filepath;
+    }
+
+    /**
+     * Returns name of file.
+     * @return
+     */
+    public String name() {
+        return name;
+    }
 
 
 }
